7b0c89946fda3e138979d6b2e98690d72a5691e5
yoshimasa_nagse <yoshimasa_nagase@ss.toyota-tsusho.com>
2018-09-12 19:43:02 +0900

    initialコミット

---
+++ src/main/java/com/superior/fw/superior/SuperiorApplication.java
@@ -0,0 +1,12 @@
+package com.superior.fw.superior;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperiorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperiorApplication.class, args);
+	}
+}
---
+++ src/test/java/com/superior/fw/superior/SuperiorApplicationTests.java
@@ -0,0 +1,16 @@
+package com.superior.fw.superior;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SuperiorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
