eda5d1fcac4f290ccbfdd069ad6bf74c44d04733
yoshimasa_nagse <yoshimasa_nagase@ss.toyota-tsusho.com>
2018-09-13 11:40:08 +0900

    コメントを追加

--- src/main/java/com/superior/fw/superior/SuperiorApplication.java
+++ src/main/java/com/superior/fw/superior/SuperiorApplication.java
@@ -3,7 +3,10 @@ package com.superior.fw.superior;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
 @SpringBootApplication
+@Slf4j
 public class SuperiorApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/superior/fw/superior/controller/HomeController.java
+++ src/main/java/com/superior/fw/superior/controller/HomeController.java
@@ -4,12 +4,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+
 @RestController
+@Slf4j
 public class HomeController {
 
 	@GetMapping
 	public String greeting(@RequestParam(required = false, defaultValue = "") String name) {
-		System.out.println("-----------------");
+		log.info("----------------------");
 		System.out.println("this is greeting");
 		return name.equals("") ? "Hey!" : "Hey, " + name + "!";
 	}
