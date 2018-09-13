6ca1a86a2d6a5fbe8875f8617a5626e8daa5b885
yoshimasa_nagse <yoshimasa_nagase@ss.toyota-tsusho.com>
2018-09-13 18:51:19 +0900

    ログ出力を追加

--- src/main/java/com/superior/fw/superior/controller/HomeController.java
+++ src/main/java/com/superior/fw/superior/controller/HomeController.java
@@ -1,6 +1,8 @@
 package com.superior.fw.superior.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,10 +12,18 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class HomeController {
 
-	@GetMapping
-	public String greeting(@RequestParam(required = false, defaultValue = "") String name) {
+	@RequestMapping
+	public String greeting(@RequestParam(required = false, defaultValue = "", value = "/xx") String name) {
 		log.info("----------------------");
-		System.out.println("this is greeting");
+		System.out.println("this is greeting...");
 		return name.equals("") ? "Hey!" : "Hey, " + name + "!";
 	}
+
+	@RequestMapping(method = RequestMethod.GET, value = "/xxx")
+	public String greeting2() {
+		log.info("----------------------");
+		System.out.println("this is greeting...");
+		return "fixed";
+	}
+
 }
